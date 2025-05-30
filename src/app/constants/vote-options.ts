ef4bd695bc33203152dac06017b0ea2322c7adf1
govindasb <gbandaru133@gmail.com>
2025-05-30 00:33:05 -0400

    added button highlight upon selecting a vote

---
+++ src/app/constants/vote-options.ts
@@ -0,0 +1,11 @@
+export const voteOptions = [
+    { value: '?', label: '?', isSelected: false },
+    { value: '0', label: '0', isSelected: false },
+  { value: '1/2', label: '1/2', isSelected: false },
+  { value: '1', label: '1', isSelected: false },
+  { value: '2', label: '2', isSelected: false },
+  { value: '3', label: '3', isSelected: false },
+  { value: '5', label: '5', isSelected: false },
+  { value: '8', label: '8', isSelected: false },
+  { value: '13', label: '13', isSelected: false },
+  { value: '100', label: '100', isSelected: false }];
